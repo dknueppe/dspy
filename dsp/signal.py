572a8d3a76cfff7942e9d52af9a5d5b1d9d26a14
Daniel Knüppe <daniel_k@posteo.net>
2019-05-22 12:45:02 +0200

    WIP

--- dsp/signal.py
+++ dsp/signal.py
@@ -8,13 +8,12 @@ def linspace(start, stop, dt):
     return np.linspace(start, stop, int(round((stop - start)/dt)) + 1, endpoint=True)
 
 def ifftfreq(freq_min, freq_max, time_frame, n=50):
-    print("ifftfreq param: ", freq_min, freq_max, time_frame, n)
     return linspace(time_frame[0], time_frame[1], 1 / ((freq_max - freq_min) / n))
 
 class Signal:
     """Represents a signal in either time or frequency domain
     """
-    version = '0.1'
+    version = '0.2'
     
     def __init__(self, x_val, y_val, domain='time', title='Signal', time_frame=None):
         if x_val.size != y_val.size:
@@ -27,12 +26,36 @@ class Signal:
         self.title = title
         self.time_frame = (x_val[0], x_val[-1]) if domain == 'time' else time_frame
 
+    @classmethod
+    def from_func(cls, func, start, stop, dt, **kwargs):
+        x = linspace(start, stop, dt)
+        y = func(x)
+        return Signal(x, y, time_frame=(start, stop))
+    
+    @classmethod
+    def from_array(array, fs = 44100):
+        stop = (array.size - 1) / fs
+        x = linspace(0, stop, 1 / fs)
+        return Signal(x, array))
+
     def __add__(self, signal):
-        common_x = Signal.common_time(self, signal)
+        common_x, timeframe = Signal.common_time(self, signal)
         sig_sum = np.zeros(common_x.size)
         sig_sum += self.padded(common_x, 0)
         sig_sum += signal.padded(common_x, 0)
-        return Signal(common_x, sig_sum)
+        return Signal(common_x, sig_sum, time_frame=timeframe)
+    
+    def __sub__(self, signal):
+        common_x, timeframe = Signal.common_time(self, signal)
+        sig_sum = np.zeros(common_x.size)
+        sig_sum -= self.padded(common_x, 0)
+        sig_sum -= signal.padded(common_x, 0)
+        return Signal(common_x, sig_sum, time_frame=timeframe)
+
+    def __mult__(self, signal):
+        common_x, timeframe = Signal.common_time(self, signal)
+        sig_prod = self.padded(common_x, 0) * signal.padded(common_x, 0)
+        return Signal(common_x, sig_prod, time_frame=timeframe)
 
     def __iter__(self):
         return self
@@ -40,6 +63,11 @@ class Signal:
     def __next__(self):
         raise StopIteration
 
+    @property
+    def size(self):
+        return self.x_val.size
+
+    @property
     def fft(self):
         x = np.fft.fftfreq(self.x_val.size, self.dt)
         y = np.fft.fft(self.y_val)
@@ -65,12 +93,6 @@ class Signal:
         back_pad = padding - front_pad
         return np.pad(self.y_val, (front_pad, back_pad), 'constant', constant_values=(fill, fill))
 
-    @classmethod
-    def from_func(cls, func, start, stop, dt, **kwargs):
-        x = linspace(start, stop, dt)
-        y = func(x)
-        return Signal(x, y)
-
     @staticmethod
     def common_time(*args):
         total_min = min(args[0].x_val)
@@ -83,7 +105,7 @@ class Signal:
             total_min = x_min if x_min < total_min else total_min
             total_max = x_max if x_max > total_max else total_max
         common_x = linspace(total_min, total_max, args[0].dt)
-        return common_x
+        return (common_x, (total_min, total_max))
 
     @staticmethod
     def plot(*args, **kwargs):
@@ -95,16 +117,16 @@ class Signal:
         if n == 1:
             fig = plt.figure(figsize=(10,90/21))
             if args[0].domain == 'frequency':
-                plt.plot(args[0].x_val, abs(np.fft.fftshift(args[0].y_val))/args[0].y_val.size)
+                plt.plot(np.fft.fftshift(args[0].x_val), abs(np.fft.fftshift(args[0].y_val))/args[0].y_val.size)
             else:
                 plt.plot(args[0].x_val, args[0].y_val)
             plt.grid(True)
             plt.title(args[0].title)
 
         else:
-            common_x = Signal.common_time(*args)
+            common_x, timeframe = Signal.common_time(*args)
             fig, subs = plt.subplots(int(n/columns), columns, figsize=(10,90/21*n/columns), sharex='col')
-            print(subs)
+            #print(subs)
 
             for signal, subplot in zip(args, subs):
                 y = signal.padded(common_x)
@@ -119,33 +141,46 @@ class Signal:
         fig.show()
 
 # Test for custom stuff
-f = lambda t: 5* np.sin(2*np.pi*t)
-t0 = linspace(-2*np.pi, 2*np.pi, 0.1)
-t1 = linspace(-2*np.pi, 0, 0.1)
-y0 = np.sin(t0)
-y1 = np.cos(t1)
-t2 = t0 + 3
-y2 = np.zeros(t2.size)
-y2[y2.size // 2 :] = 1
-foo = Signal(t0, y0 * 5, title='Sinus')
-bar = Signal(t2, y2 * 5, title='Heaviside')
-foobar = Signal.from_func(f, -3*np.pi, 3*np.pi, 0.1)
-Signal.plot(foo)
-Signal.plot(foo, bar, foobar, foobar + foo + bar)
+#f = lambda t: 5* np.sin(2*np.pi*t)
+#t0 = linspace(-2*np.pi, 2*np.pi, 0.1)
+#t1 = linspace(-2*np.pi, 0, 0.1)
+#y0 = np.sin(t0)
+#y1 = np.cos(t1)
+#t2 = t0 + 3
+#y2 = np.zeros(t2.size)
+#y2[y2.size // 2 :] = 1
+#foo = Signal(t0, y0 * 5, title='Sinus')
+#bar = Signal(t2, y2 * 5, title='Heaviside')
+#foobar = Signal.from_func(f, -3*np.pi, 3*np.pi, 0.1)
+#Signal.plot(foo)
+#Signal.plot(foo, bar, foobar, foobar + foo + bar)
+
+def cos_add():
+    t = linspace(-0.1, 0.1, 0.0002)
+    f = lambda k : (1 / (k + 1)) * np.cos(2 * np.pi * k * 250 * t)
+    y = np.zeros(t.size)
+    for n in range(6):
+        y += f(n)
+    return Signal(t, y, time_frame=(-0.1, 0.1))
+fourier = cos_add()
+Signal.plot(fourier)
+fft = fourier.fft
+Signal.plot(fft)
+
 #print(foo)
 #print(foobar.dt)
-t2 = linspace(-3*np.pi, 3*np.pi, 0.1)
-test = Signal(t2, f(t2))
+#t2 = linspace(-3*np.pi, 3*np.pi, 0.1)
+#test = Signal(t2, f(t2))
 #selection = (foo, foo.fft)
-Signal.plot(foobar.fft(), columns=2)
+#Signal.plot(foobar.fft(), columns=2)
 #Signal.plot(test)
 #Signal.plot(test.fft())
 #Signal.plot((test, test.fft()),columns=2)
-test_time = linspace(0, 3, 0.0003)
-test_freq = np.fft.fftfreq(test_time.size, 1/np.mean(np.diff(test_time)))
-print("dt from test_freq = ", np.mean(np.diff(np.fft.fftshift(test_freq))))
-test_time_from_freq = ifftfreq(min(test_freq), max(test_freq), (0,3), test_freq.size)
-print("test_time_from_freq = ", test_time_from_freq)
+#test_time = linspace(0, 3, 0.0003)
+#test_freq = np.fft.fftfreq(test_time.size, 1/np.mean(np.diff(test_time)))
+#print("dt from test_freq = ", np.mean(np.diff(np.fft.fftshift(test_freq))))
+#test_time_from_freq = ifftfreq(min(test_freq), max(test_freq), (0,3), test_freq.size)
+#print("test_time_from_freq = ", test_time_from_freq)
 
 #DTMF
 upper_freq = (1029, 1336, 1477)
@@ -168,4 +203,8 @@ output = 20 * np.sin(2*np.pi*tone*t)
 
 # playing .wav
 FSample, samples = wav.read('/home/d/Downloads/audio_2.wav')
+print(samples.size)
+t = np.linspace(0, samples.size / FSample, samples.size)
+audio = Signal(t, samples)
+Signal.plot(audio)
 sd.play(samples, FSample)
